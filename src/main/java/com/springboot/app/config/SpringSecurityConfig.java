8847293c38f86e2b206e44a847c6112a192a432c
cristian osorio <cris300oso@gmail.com>
2022-08-11 09:15:26 -0500

    seurity

--- src/main/java/com/springboot/app/config/SpringSecurityConfig.java
+++ src/main/java/com/springboot/app/config/SpringSecurityConfig.java
@@ -8,10 +8,7 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-import org.springframework.security.core.userdetails.User;
-import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
-import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
 
 @EnableGlobalMethodSecurity(securedEnabled = true)
